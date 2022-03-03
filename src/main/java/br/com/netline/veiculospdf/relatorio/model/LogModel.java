ef884d606bf374a3fef77c8ac5f9530ce93a43a4
Hugo <hugoabrantesd@gmail.com>
2022-03-03 15:41:30 -0300

    Adicionando campo id para ordenação dos dados no banco de dados

--- src/main/java/br/com/netline/veiculospdf/relatorio/model/LogModel.java
+++ src/main/java/br/com/netline/veiculospdf/relatorio/model/LogModel.java
@@ -16,6 +16,9 @@ public class LogModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true)
     private UUID uuid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String emailUser;
     private String data;
     private String action;
