349caf6172ad918ec8a57d5ca6289f7ebb47a836
Hugo <hugoabrantesd@gmail.com>
2022-03-03 16:11:02 -0300

    Resolvendo bug

--- src/main/java/br/com/netline/veiculospdf/relatorio/model/LogModel.java
+++ src/main/java/br/com/netline/veiculospdf/relatorio/model/LogModel.java
@@ -15,11 +15,13 @@ public class LogModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true)
-    private UUID uuid;
+    private Integer id;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    @Column(unique = true)
+    private UUID uuid;
+
     private String emailUser;
     private String data;
     private String action;
